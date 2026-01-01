7161c4dcdee8427c44c82b554679cf6924f2af78
Halilintar Daiva <daiva@halilintar.my.id>
2026-01-01 13:35:52 +0700

    feat: init untitledui

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type {Metadata, Viewport} from "next";
+import {Inter} from "next/font/google";
+import {RouteProvider} from "@/providers/router-provider";
+import {Theme} from "@/providers/theme";
+import "@/styles/globals.css";
+
+const inter = Inter({
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+    title: "Your App Name",
+    description: "Your app description",
+};
+
+export const viewport: Viewport = {
+    colorScheme: "light",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" className={`${inter.variable} scroll-smooth`} suppressHydrationWarning>
+        <body className="bg-primary antialiased">
+        <RouteProvider>
+            <Theme>
+                {children}
+            </Theme>
+        </RouteProvider>
+        </body>
+        </html>
+    );
+}
+
---
+++ src/app/test/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import {Button} from "@/components/base/buttons/button";
+import {Check} from "@untitledui/icons";
+import {useState} from "react";
+import {getLocalTimeZone, today} from "@internationalized/date";
+import {DatePicker} from "@/components/application/date-picker/date-picker";
+import {DateValue} from "react-aria-components";
+
+const now = today(getLocalTimeZone());
+
+export default function Test() {
+
+    const [value, setValue] = useState<DateValue | null>(now);
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            hello
+            <Button color="primary" size="md" iconLeading={<Check data-icon />}>Publish now</Button>
+            <DatePicker aria-label="Date picker" value={value} onChange={setValue} />
+        </div>
+    );
+}
