6d7fb902354fe12a597c52e4f0cd4c115339a812
Halilintar Daiva <daiva@halilintar.my.id>
2026-01-03 10:22:26 +0700

    feat: init prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,36 @@
-import type {Metadata, Viewport} from "next";
-import {Inter} from "next/font/google";
-import {RouteProvider} from "@/providers/router-provider";
-import {Theme} from "@/providers/theme";
-import "@/styles/globals.css";
+import type { Metadata, Viewport } from 'next';
+import { Inter } from 'next/font/google';
+import { RouteProvider } from '@/providers/router-provider';
+import { Theme } from '@/providers/theme';
+import '@/styles/globals.css';
 
 const inter = Inter({
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-inter",
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
-    title: "Your App Name",
-    description: "Your app description",
+  title: 'Your App Name',
+  description: 'Your app description',
 };
 
 export const viewport: Viewport = {
-    colorScheme: "light",
+  colorScheme: 'light',
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" className={`${inter.variable} scroll-smooth`} suppressHydrationWarning>
-        <body className="bg-primary antialiased">
+  return (
+    <html lang="en" className={`${inter.variable} scroll-smooth`} suppressHydrationWarning>
+      <body className="bg-primary antialiased">
         <RouteProvider>
-            <Theme>
-                {children}
-            </Theme>
+          <Theme>{children}</Theme>
         </RouteProvider>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
-
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,24 +1,25 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import {Button} from "@/components/base/buttons/button";
-import {Check} from "@untitledui/icons";
-import {useState} from "react";
-import {getLocalTimeZone, today} from "@internationalized/date";
-import {DatePicker} from "@/components/application/date-picker/date-picker";
-import {DateValue} from "react-aria-components";
+import Image from 'next/image';
+import { Button } from '@/components/base/buttons/button';
+import { Check } from '@untitledui/icons';
+import { useState } from 'react';
+import { getLocalTimeZone, today } from '@internationalized/date';
+import { DatePicker } from '@/components/application/date-picker/date-picker';
+import { DateValue } from 'react-aria-components';
 
 const now = today(getLocalTimeZone());
 
 export default function Test() {
+  const [value, setValue] = useState<DateValue | null>(now);
 
-    const [value, setValue] = useState<DateValue | null>(now);
-
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            hello
-            <Button color="primary" size="md" iconLeading={<Check data-icon />}>Publish now</Button>
-            <DatePicker aria-label="Date picker" value={value} onChange={setValue} />
-        </div>
-    );
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      hello
+      <Button color="primary" size="md" iconLeading={<Check data-icon />}>
+        Publish now
+      </Button>
+      <DatePicker aria-label="Date picker" value={value} onChange={setValue} />
+    </div>
+  );
 }
